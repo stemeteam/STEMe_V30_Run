08fbdcbd5c5cc2374184945439db39c5441ee2b7
stemeteam <steme.team@gmail.com>
2023-12-25 22:22:54 +0700

    new repo

---
+++ device/config.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+sudo mount --bind /home/pi/Desktop/cpuinfo /proc/cpuinfo
+mkdir /home/pi/Desktop/hello
+
---
+++ device/runConfig64bit.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+#command copy file rc.local to /etc
+sudo cp /home/pi/Desktop/STEMe_V30_Run/device/rc.local /etc/
+#command set arm_64bit to config.txt
+sudo cp /home/pi/Desktop/STEMe_V30_Run/device/config.txt /boot/
